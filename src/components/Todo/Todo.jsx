05c1aac347acfcbbfe4ef0a59162e6f1c9707892
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-01-04 22:27:10 -0800

    add-data

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,23 +1,22 @@
-import React from 'react'
-import FeatherIcon from 'feather-icons-react'
-const Todo = () => {
-    return (
-<div className='todos-todo'>
-    <div className='todo-icon'>
-    <FeatherIcon icon="circle"/>    
-     </div>
-    <div className='todo-text'> مهمه 1</div>
-    <div className='todo-cta'>
-        <div className='todo-cta-edit'>
-            <FeatherIcon icon="edit"/>
+import React from "react";
+import FeatherIcon from "feather-icons-react";
+const Todo = ({ todo, toggelTodo }) => {
+  return (
+    <div className={`todos-todo ${todo.done ? "done" : ""}`}>
+      <div className="todo-icon" onClick={() => toggelTodo(todo.id)}>   
+        <FeatherIcon icon={todo.done ? "check-circle" : "circle"} />
+      </div>
+      <div className="todo-text">{todo.title}</div>
+      <div className="todo-cta">
+        <div className="todo-cta-edit">
+          <FeatherIcon icon="edit" />
         </div>
-        <div className='todo-cta-delete'> 
-        <FeatherIcon icon="trash-2"/>
+        <div className="todo-cta-delete">
+          <FeatherIcon icon="trash-2" />
         </div>
+      </div>
     </div>
+  );
+};
 
-</div>
-    )
-}
-
-export default Todo
+export default Todo;
